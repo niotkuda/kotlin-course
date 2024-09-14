37139bb3ee064c7709b4b67e4fefbf00514f4b5c
juliana <proninajuliana2517@gmail.com>
2024-09-15 00:49:17 +0300

    homework lesson4

---
+++ src/main/kotlin/org/niotkuda/kotlincourse/lesson4/homework4.kt
@@ -0,0 +1,81 @@
+package org.niotkuda.kotlincourse.lesson4
+
+val myInt: Int = 42 //or byte? (-128.127)
+
+val myLong: Long = 98765432123456789L //в нём же принято хранить секунды, автоматически присвоит если в конце "L"
+
+val myFloat: Float = 23.45f //плавающая точка получается
+
+val myInt: Int = 0.123456789
+
+val text: String = "Kotlin & Java"
+
+val isKotlinFun: Boolean = false //only on/off; 1/0
+
+val letter: Char = 'c'
+
+val myInt: Int = 500 //но для оптимизации можно использовать Short (+-32768)
+
+val myLong: Long = 4294967296L
+
+val myFloat: Float = 18.0f
+
+val myInt: Int = -0.001 //???????
+
+val text: String = "OpenAI"
+
+val isKotlinFun: Boolean = true //убрала кавычки /any
+
+val letter: Char = '9'
+
+val myInt: Int = 2048
+
+val myLong: Long = 10000000000L
+
+val myFloat: Float = 5.75f
+
+val anything: Any = 1.414 //тип будет известен в момент runtime
+
+val text: String = "Artificial Intelligence"
+
+val isKotlinFun: Boolean = false
+
+val letter: Char = '@'
+
+val myInt: Int = 1024
+
+val myLong: Long = 1234567890123L
+
+val myFloat: Float = 10.01f
+
+val myInt: Int = -273.15
+
+val text: String = "SpaceX"
+
+val isKotlinFun: Boolean = true
+
+val text: String = '🤯' //как узнать из чего состоит смайлик, у некоторых же несколько символов или имеет значение только сколькими отображается??
+
+val text: String = "65535" //исправила кавычки
+
+val myLong: Long = 72057594037927935L //проверить
+
+val myFloat: Float = 2.71828f
+
+val myInt: Int = 101.0101
+
+val text: String = "Quantum Computing"
+
+val isKotlinFun: Boolean = false
+
+val letter: Char = 'x'
+
+val myInt: Int = 314
+
+val myLong: Long = 123456789123456789L
+
+val myFloat: Float = 6.626f
+
+val myInt: Int = 0.007
+
+val text: String = "Android Studio"
