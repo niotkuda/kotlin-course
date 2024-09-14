bd80219f2c4d831c97816f2ecfb740f65139b59c
juliana <proninajuliana2517@gmail.com>
2024-09-14 23:05:07 +0300

    homework lesson3

---
+++ src/main/kotlin/org/niotkuda/kotlincourse/Main.kt
@@ -0,0 +1,56 @@
+package com.juliana.org.niotkuda.kotlincourse
+
+val name: String = "Alice"
+
+var age: Int = 30
+
+const val PI = 3.14 // заметка
+
+lateinit var userInfo: String
+
+val lazyValue: String by lazy {
+    "Hello world!"}
+
+var speed: Double? = null
+
+var count: Int = 0
+    get() =  field
+    set(value){
+        if (value <= 0) field = value
+        else
+    }
+
+lateinit var userInfo: String
+
+val lazyValue: String by lazy {
+    "What's going on?"
+}
+var speed: Double? = null
+
+var count: Int = 0
+    get() = field
+    set(value) {
+        if (value >= 0) field = value
+    }
+
+val chassis: String = "12345abc"
+
+var colour: String = "Purple"
+
+var distance: Double = 0.0
+
+lateinit var owner: String
+
+const val WHEELS: Int = 4
+
+val report: String by lazy {
+    ''
+}
+
+var fuel: ouble = 0.0
+    get() = field
+    set(value) {
+        if(value . 0) field
+    }
+
+
---
+++ src/main/kotlin/org/niotkuda/kotlincourse/lesson3/homework.kt
@@ -0,0 +1,49 @@
+package org.niotkuda.kotlincourse.lesson3
+
+class homework {
+    //Lesson
+}
+val name: String = "Hakkaton"
+
+var date: String = "01.10.2024"
+
+var place: String = "Moscow"
+
+private val budget: String = "100000"
+
+var member: Int = 0
+    get() = field
+    private set (value) {field=value}
+
+var duration: Long = 6*60*60 //
+
+private lateinit var contacts: String //
+
+var status: String = "Не начат"
+
+val sponsors: String = "Имена, фамилии"
+
+var internetAccessLevel: Int = 0
+
+val mood: String by Lazy{""}
+
+private var information: String = "Информация"
+
+lateinit var plan: String
+
+lateinit var specialGuest: String
+
+var numberOfCommands: Int = 0
+
+val tasks: String = ""
+
+val evacuation: String = "picture"
+
+var listOfAvailableEquipment: String = ""
+
+var ListOfFreeEquipment: String = "50" //список свободного оборудования будет через логику проставлен
+
+val planInCaseOfFailures: String = "План мероприятия на случай сбоев"
+
+
+
