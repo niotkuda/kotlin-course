b12275a711fe32cc17acdf67cb203419dbb978ec
juliana <proninajuliana2517@gmail.com>
2024-09-22 00:53:30 +0300

    homework lesson5. first three blocks

--- src/main/kotlin/org/niotkuda/kotlincourse/lesson5/operatorElvis.kt
+++ src/main/kotlin/org/niotkuda/kotlincourse/lesson5/operatorElvis.kt
@@ -2,15 +2,29 @@ package org.niotkuda.kotlincourse.lesson5
 //1 task
 fun main() {
     val initialIntensity = 100 //интенсивность начальная пропорциональна звуку после затухания
+   val coefficient = 0.5
 
+    val cargoValue: Double? = null //double null
 
-}
-
-// узнать полную цену доставки груза. Доставка 50+0.5%(страховка)
-fun main(){
-
-    val cargoValue: Double? = null
     val totalCost = calculateTotalShippingCost(cargoValue)
 
-    println("Полная стоимость доставкиЖ $${"%.2f"}")
+    println("Полная стоимость доставки $${"%.2f".format(totalCost)}")
+    println(calculateTotalShippingCost(70.0) )
 }
+
+//рассчитать предполаг.инт.звука после затухания
+
+
+// узнать полную цену доставки груза. Доставка 50+0.5%(страховка)
+
+
+
+fun calculateTotalShippingCost(cargoValue: Double?): Double {
+
+    val effectiveCargoValue = cargoValue ?: 50.0
+    val insurance = effectiveCargoValue * 0.005
+    val totalShippingCost = effectiveCargoValue + insurance
+    return totalShippingCost
+
+}
+
