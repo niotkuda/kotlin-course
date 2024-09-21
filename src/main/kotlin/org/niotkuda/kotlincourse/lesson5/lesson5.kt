d94fc0cb9cc41986336a2746c41172c14a36a372
juliana <proninajuliana2517@gmail.com>
2024-09-21 22:23:49 +0300

    homework lesson5. first three blocks

--- src/main/kotlin/org/niotkuda/kotlincourse/lesson5/lesson5.kt
+++ src/main/kotlin/org/niotkuda/kotlincourse/lesson5/lesson5.kt
@@ -1,34 +1,18 @@
-package org.niotkuda.kotlincourse.lesson5
 
-fun main() {
-val sum = "10" + "5"
-val diff = 10 - 5
-val product = 10 * 5
-val quotient = 10 / 5
-val remainder = 10 % 5 //остаток от деления...
-val remainder1 = 10 % 6
-val remainder2 = 10 % 4
-val isEqual = 5 == 4
-val isNotEqual = (5 != 5)
-val isGreater = (5 > 3)
-val isLesser = (5 < 3)
-val isGreaterOrEqual = (5 >= 5)
-val isLesserOrEqual = (5 <= 3)
-val andResult = false && false
-val orResult = true || false
-val notResult = !(5 > 3)
-var number = 5
-number += 3
-number -= 2
 
-printIn(number--)
-    
-}
-
-fun printIn(number: Int) {
+fun main () {
+    printSiteLang( lang: "Rus" )
+    printSiteLang( lang: null )
 
 }
+fun printPrice(price: Double, s: Int?){
+    val koef = (100 - (s ?: 0))/100.0
+    println(price * koef)
+}
 
+fun printSiteLang(lang: String?){
+    val defaultLang: String = "Eng"
+    println(lang ?: defaultLang)
+}
 
-
-
+fun printBoxStuff(stuff: String?)
