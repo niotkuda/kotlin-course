c47256f42b30191f5f30e48c394a59d98c60e7bc
juliana <proninajuliana2517@gmail.com>
2024-09-22 15:36:26 +0300

    homework lesson5. first three blocks

---
+++ src/main/kotlin/org/niotkuda/kotlincourse/lesson6/homework6.kt
@@ -0,0 +1,19 @@
+package org.niotkuda.kotlincourse.lesson6
+
+fun main(){
+    val intRange1: IntRange = 3..5
+    println("Spring")
+    val winter: Int = 12
+    println("Winter")
+    val intRange2: IntRange = 1..2
+    println("Spring")
+    val intRange3: IntRange = 6..8
+    println("Summer")
+    val intRange4: IntRange = 2..4
+    println("Autumn")
+
+    fun checkSeasons(intRange: IntRange)
+
+}          //Задание 1: "Определение Сезона"
+//Контекст: Напишите функцию, которая на основе номера месяца возвращает сезон года.
+
