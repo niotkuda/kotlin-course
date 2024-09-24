60371f65e5a1ae75918dc18e885ebe20da9b6d6a
juliana <proninajuliana2517@gmail.com>
2024-09-24 21:51:46 +0300

    homework lesson6.

--- src/main/kotlin/org/niotkuda/kotlincourse/lesson5/operatorElvis.kt
+++ src/main/kotlin/org/niotkuda/kotlincourse/lesson5/operatorElvis.kt
@@ -1,46 +1,46 @@
-package org.niotkuda.kotlincourse.lesson5
-//1 task
-fun main() {
-    val initialIntensity = 100 //интенсивность начальная пропорциональна звуку после затухания
-    val knowDampingCoefficient: Double = 0.7 //коэф. затухания
-
-
-    val coefficient = 0.5
-
-
-    val intensityWithKnownCoefficient = calculateDampedSoundIntensity(initialIntensity)
-
-    println("Интенсивность звука с известным коэффициентом затухания: $intensityWithKnownCoefficient")
-
-    val cargoValue: Double? = null //double null
-
-
-//    val unknownDampingCoefficient: Double? = null //коэф.отсутствует
-
-    fun intensityWithUnknownCoefficient = calculateDampedSoundIntensity(initialIntensity: Double, Double?): Double
-    println("Интенсивность звука с неизвестным коэффициентом затухания: ${intensityWithUnknownCoefficient()}")
-
-
-    val totalCost = calculateTotalShippingCost(cargoValue)
-
-    println("Полная стоимость доставки $${"%.2f".format(totalCost)}")
-    println(calculateTotalShippingCost(70.0) )
-}
-
-fun calculateDampedSoundIntensity(initialIntensity: Double, dampingCoefficient: Double?): {
-    val effectiveDampingCoefficient = dampingCoefficient ?: 0.5
-    return initialIntensity * effectiveDampingCoefficient
-}
-// узнать полную цену доставки груза. Доставка 50+0.5%(страховка)
-
-
-
-fun calculateTotalShippingCost(cargoValue: Double?): Double {
-
-    val effectiveCargoValue = cargoValue ?: 50.0
-    val insurance = effectiveCargoValue * 0.005
-    val totalShippingCost = effectiveCargoValue + insurance
-    return totalShippingCost
-
-}
-
+//package org.niotkuda.kotlincourse.lesson5
+////1 task
+//fun main1() {
+//    val initialIntensity = 100 //интенсивность начальная пропорциональна звуку после затухания
+//    val knowDampingCoefficient: Double = 0.7 //коэф. затухания
+//
+//
+//    val coefficient = 0.5
+//
+//
+//    val intensityWithKnownCoefficient = calculateDampedSoundIntensity (initialIntensity)
+//
+//    println("Интенсивность звука с известным коэффициентом затухания: $intensityWithKnownCoefficient")
+//
+//    val cargoValue: Double? = null //double null
+//
+//
+////    val unknownDampingCoefficient: Double? = null //коэф.отсутствует
+//
+//    fun intensityWithUnknownCoefficient = calculateDampedSoundIntensity(initialIntensity: Double):
+//    println("Интенсивность звука с неизвестным коэффициентом затухания: ${intensityWithUnknownCoefficient()}")
+//
+//
+//    val totalCost = calculateTotalShippingCost(cargoValue)
+//
+//    println("Полная стоимость доставки $${"%.2f".format(totalCost)}")
+//    println(calculateTotalShippingCost(70.0) )
+//}
+//
+//fun calculateDampedSoundIntensity(initialIntensity: Double, dampingCoefficient: Double?): {
+//    val effectiveDampingCoefficient = dampingCoefficient ?: 0.5
+//    return initialIntensity * effectiveDampingCoefficient
+//}
+//// узнать полную цену доставки груза. Доставка 50+0.5%(страховка)
+//
+//
+//
+//fun calculateTotalShippingCost(cargoValue: Double?): Double {
+//
+//    val effectiveCargoValue = cargoValue ?: 50.0
+//    val insurance = effectiveCargoValue * 0.005
+//    val totalShippingCost = effectiveCargoValue + insurance
+//    return totalShippingCost
+//
+//}
+//
--- src/main/kotlin/org/niotkuda/kotlincourse/lesson6/lesson6.kt
+++ src/main/kotlin/org/niotkuda/kotlincourse/lesson6/lesson6.kt
@@ -1,2 +1,17 @@
-package org.niotkuda.kotlincourse.lesson6
-
+//package org.niotkuda.kotlincourse.lesson6
+//
+//fun main4(){
+//    val number = 4
+//    if (number > 5){
+//        println("Число больше 5")
+//    } else {
+//        println("Число меньше или равно 5")
+//    }
+//}
+//if (условие1) {
+//    //код,если условие1 истинно
+//}else if (условие2){
+//    //код,если условие2 истинно
+//}else{
+//    //код, если оба условия ложны
+//}
